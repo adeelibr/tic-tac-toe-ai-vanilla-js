7d4612ec698a8b02de60fe240f3cd0216a0d9aa0
adeelibr <adeelimranr@gmail.com>
2018-04-29 18:37:56 +0500

    working

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let origBoard;
-const huPlayer = '0';
-const aiPlayer = 'x';
+const HUMAN_PLAYER = '0';
+const AI_PLAYER    = 'x';
 
 const winCombos = [
   [0, 1, 2],
@@ -16,9 +16,27 @@ const winCombos = [
 ];
 
 const cells = document.getElementsByClassName('cell');
-startGame();
+onStartGame();
 
-function startGame () {
+function onStartGame () {
+  // console.log('Starting Game');
   document.querySelector('.end-game').style.display = 'none';
   origBoard = Array.from(Array(9).keys());
+  // console.table(origBoard);
+  // console.log(cells);
+  for(let i=0; i< cells.length; i++) {
+    cells[i].innerText = '';
+    cells[i].style.removeProperty('background-color');
+    cells[i].addEventListener('click', onTurnClick, false)
+  }
 };
+
+function onTurnClick (e) {
+  // console.log(e.target.id);
+  const { id:squareId } = e.target;
+  onTurn(squareId, HUMAN_PLAYER);
+}
+
+function onTurn (squareId, player) {
+  document.getElementById(squareId).innerText = player;
+}
